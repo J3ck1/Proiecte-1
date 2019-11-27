bc51ac6760545e8b524d3b743d711be095fcedc1
J3ck1 <50956574+J3ck1@users.noreply.github.com>
2019-11-27 20:23:53 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,41 +1,64 @@
 #include <iostream>
-#include "funct.h"
-using namespace std;
 
-int st[50], nr_sol;
+using namespace std;
+int m, c, st[100], date[50][5];
+
+//  1 = 1m;
+//  2 = 1c;
+//  3 = 2m;
+//  4 = 2c;
+//  5 = 1c 1m;
+
+void afisare(int k)
+{
+    for(int i=1; i<=k; i++)
+        cout << st[i] << endl;
+
+}
+
+void conv(int k, int v, int x)
+{
+    if(v == 1) date[k][x+1]+=1;
+    if(v == 2) date[k][x+2]+=1;
+    if(v == 3) date[k][x+1]+=2;
+    if(v == 4) date[k][x+2]+=2;
+    if(v == 5) {
+            date[k][x+2]+=1;
+            date[k][x+1]+=1;
+    }
+}
+
+bool valid(int k)
+{
+    if(k%2 == 1)
+        conv(k, st[k], 2);
+    else
+        conv(k, st[k], 0);
+    if(date[k][1] < date[k][2] && date[k][1] != 0) return 0;
+    if(date[k][3] < date[k][4] && date[k][3] != 0) return 0;
+    return 1;
+}
 
 void backy()
 {
   int k = 1;
   st[k] = 0;
-  while (k>0)
-    if (st[k]<n)
+  while (k > 0)
+    if (st[k] < 5)
     {
       st[k]++;
       if (valid(k))
-        if(k == n) {
-                k = 4;
-                indentificator_de_valoare();
-        }
+        if(date[k][1] == 0)
+            afisare(k);
         else st[++k]=0;
     }
     else k--;
 }
-
 int main()
 {
-    citire();
+    cin >> m >> c;
+    date[0][1] = m;
+    date[0][2] = c;
     backy();
-    for (int i=1; i<=cont; i++)
-    {
-        if (v2[i].nr == m) {
-        cout << "P1: " << v[v2[i].a].x << " " << v[v2[i].a].y << endl;
-        cout << "P2: " << v[v2[i].b].x << " " << v[v2[i].b].y << endl;
-        cout << "P3: " << v[v2[i].c].x << " " << v[v2[i].c].y << endl;
-        cout << "P4: " << v[v2[i].d].x << " " << v[v2[i].d].y << endl;
-        }
-        cout << endl;
-    }
-    cout << v2[cont].nr;
     return 0;
 }
